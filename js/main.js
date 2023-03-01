c984dd26807232b97b49b01831103f6b0f26e8f8
Valentino Errandonea <valentinoerrandonea2002@gmail.com>
2023-03-01 12:56:42 -0300

    first commit :)

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//How to make an HTTP request with JavaScript?
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+
+
+
+//Source: https://stackoverflow.com/questions/247483
+
+
+
+
+
+
+
+
+
+
